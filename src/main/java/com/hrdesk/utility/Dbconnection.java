d1116d4c76c49ca379268bf3bb7a3a8d8c32e821
Vijay417-sys <hosapetivijay18@gmail.com>
2026-07-07 21:53:30 +0530

    ui

--- src/main/java/com/hrdesk/utility/Dbconnection.java
+++ src/main/java/com/hrdesk/utility/Dbconnection.java
@@ -5,30 +5,26 @@ import java.sql.DriverManager;
 
 public class Dbconnection {
 
+	private static final String URL = "jdbc:mysql://localhost:3306/hrdesk_db";
 
-	    private static final String URL =
-	        "jdbc:mysql://localhost:3306/hrdesk_db";
+	private static final String USER = "root";
 
-	    private static final String USER = "root";
+	private static final String PASSWORD = "Rashivu@246";
 
-	    private static final String PASSWORD = "root";
+	public static Connection getConnection() {
 
-	    public static Connection getConnection() {
+		try {
 
-	        try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 
-	            Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(URL, USER, PASSWORD);
 
-	            return DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (Exception e) {
 
-	        } catch (Exception e) {
+			e.printStackTrace();
 
-	            e.printStackTrace();
+		}
 
-	        }
-
-	        return null;
-	    }
+		return null;
 	}
-
-
+}
