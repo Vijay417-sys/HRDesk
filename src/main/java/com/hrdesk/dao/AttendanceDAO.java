1d1319311469652ebdad9395155abdbf2d43acc5
Yuvarajrathod26 <yuvarjrathod807@gmail.com>
2026-07-09 13:56:04 +0530

    Updated AttendanceDAO and Dbconnection

--- src/main/java/com/hrdesk/dao/AttendanceDAO.java
+++ src/main/java/com/hrdesk/dao/AttendanceDAO.java
@@ -19,4 +19,5 @@ public interface AttendanceDAO {
     List<Attendance> getAttendanceByDate(Date attendanceDate);
 
     List<Attendance> getAllAttendance();
+    
 }
--- src/main/java/com/hrdesk/utility/Dbconnection.java
+++ src/main/java/com/hrdesk/utility/Dbconnection.java
@@ -26,5 +26,6 @@ public class Dbconnection {
 		}
 
 		return null;
+		System.out.println("hi");
 	}
 }
